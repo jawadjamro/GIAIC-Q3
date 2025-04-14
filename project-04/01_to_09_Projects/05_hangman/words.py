b2bcf4de5d18bf7eaf5a34b6ad3e659b589f9870
Jawad Hassan <159148373+jawadjamro@users.noreply.github.com>
2025-04-14 22:26:19 +0500

    project-04

---
+++ project-04/01_to_09_Projects/05_hangman/words.py
@@ -0,0 +1,6 @@
+words = [
+    'interface', 'compiler', 'encryption', 'frontend', 'backend',
+    'framework', 'syntax', 'loop', 'condition', 'object',
+    'inheritance', 'constructor', 'array', 'module', 'package',
+    'runtime', 'virtual', 'boolean', 'integer', 'exception'
+] 
